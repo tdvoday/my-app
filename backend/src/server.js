45d0458cc1787d45821a1a4a070458ba05dda035
Vo Hoang Phuc <vohgphuc@gmail.com>
2026-02-25 23:26:04 +0700

    init backend and project setup

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+connectDB();
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
